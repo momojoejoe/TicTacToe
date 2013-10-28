acadb0a7b15feb44e4723a0b6296ce5f0c143328
sindris12 <sindri89@gmail.com>
2013-10-28 01:52:44 +0000

    Made the functions makeGame and a function to print out the status of the game

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,6 +1,58 @@
 public class TicTacToe {
+
+	public static final int ROWS = 3, COLUMS = 3;
+	public static final int[][] board = new int[ROWS][COLUMS];
+	public static final int X = 0;
+	public static final int O = 1;
+	public static final int Start = 2;
+
 	public static void main(String[] args) {
-		System.out.println("hello");
+		System.out.println("-------------"); 
+		System.out.println("--TicTacToe--");
+		System.out.println("-------------");
+		System.out.println();
+
+		makeGame();
+
+		board[2][1] = 0;
+
+		print();
 	}
 
+	public static void makeGame()
+	{
+		for(int i = 0; i < ROWS; i++)
+		{
+			for(int j = 0; j < COLUMS; j++)
+			{
+				board[i][j] = Start;
+			}
+		}
+	}
+
+	public static void print()
+	{
+		for(int i = 0; i < ROWS; i++)
+		{
+			System.out.print("|");
+			for(int j = 0; j < COLUMS; j++)
+			{
+				if(board[i][j] == X)
+				{
+					System.out.print(" X ");
+				}
+				else if(board[i][j] == 0)
+				{	
+					System.out.print(" 0 ");
+				}
+				else
+				{
+					System.out.print(" - ");
+				}
+				System.out.print("|");
+			}
+			System.out.println();
+			System.out.println("-------------");
+		}
+	}
 }
