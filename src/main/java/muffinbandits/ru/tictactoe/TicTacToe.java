938149275813e1760050c83eb8388b001f62c729
arnif <arnifa@gmail.com>
2013-11-15 02:34:11 +0000

    now you have index.html and can multiply 2 numbers

--- src/main/java/muffinbandits/ru/tictactoe/TicTacToe.java
+++ src/main/java/muffinbandits/ru/tictactoe/TicTacToe.java
@@ -22,21 +22,18 @@ public class TicTacToe {
 	public static void main(String[] args)
 	{
 
+    staticFileLocation("/public");
+        
+        // setPort(Integer.valueOf(System.getenv("PORT"))); // broken!?!?
 
-
-		get(new Route("/hello") {
-         @Override
-         public Object handle(Request request, Response response) {
-            return "<a href='newgame'>Nyr leikur</a>";
-         }
-      });
-
-		get(new Route("/newgame") {
-         @Override
-         public Object handle(Request request, Response response) {
-            return "<h1>Teiknabord</h1>";
-         }
-      });
+        post(new Route("/add") {
+            @Override
+            public Object handle(Request request, Response response) {
+                Integer a = Integer.valueOf(request.queryParams("a"));
+                Integer b = Integer.valueOf(request.queryParams("b"));
+                return a + b;
+            }
+        });
 
 
 		// System.out.println("-------------"); 
@@ -168,7 +165,5 @@ public class TicTacToe {
 		
 		}
 	}
-
-
 
 }
