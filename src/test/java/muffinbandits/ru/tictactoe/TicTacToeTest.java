6926dfb50ad51fd06d413748999e954361226c42
arnif <arnifa@gmail.com>
2013-11-22 15:19:55 +0000

    test for TicTacToe class

---
+++ src/test/java/muffinbandits/ru/tictactoe/TicTacToeTest.java
@@ -0,0 +1,32 @@
+package muffinbandits.ru.tictactoe;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Created by: arni11, bjarnthor12, sigruns12, sindris12, sindri12, theodor11 & thordurt12
+ * Copyright (c) 2013
+ * Project: TicTacToe
+ * Package: muffinbandits.ru.tictactoe
+ * Date: 19/11/13
+ */
+
+/**
+ * Tests for the TicTacToe class.
+ */
+public class TicTacToeTest {
+
+	/**
+	 * Test TicTacToe.
+	 */
+	@Test
+    public void TestTicTacToe() {
+
+		TicTacToe tic = new TicTacToe();
+
+        assertEquals(1, 1);
+
+        }
+
+}
